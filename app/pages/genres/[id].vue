74c508d7619f745bde489dcf634b5e9a8c5b7b51
Habib Shaban <h.shaban234@yahoo.com>
2025-12-12 12:13:47 +0200

    feat: update genre details page to display genre information dynamically

--- app/pages/genres/[id].vue
+++ app/pages/genres/[id].vue
@@ -2,10 +2,13 @@
 const route = useRoute();
 const genreId = Number(route.params.id);
 
+const { genre } = useGenre(genreId);
+
 </script>
 
 <template>
   <div class="p-6">
-    <h1 class="text-3xl font-bold text-white mb-6">Genre Details: {{ genreId }}</h1>
+    <h1 class="text-3xl font-bold text-white mb-6">Genre Details</h1>
+    <GenreItem v-if="genre" :genre="genre" />
   </div>
 </template>
