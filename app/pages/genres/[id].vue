e024cd178d6d6ad961017569aec38baa40868513
Habib Shaban <h.shaban234@yahoo.com>
2025-12-12 12:09:50 +0200

    feat: create genre details page with dynamic routing

---
+++ app/pages/genres/[id].vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const route = useRoute();
+const genreId = Number(route.params.id);
+
+</script>
+
+<template>
+  <div class="p-6">
+    <h1 class="text-3xl font-bold text-white mb-6">Genre Details: {{ genreId }}</h1>
+  </div>
+</template>
