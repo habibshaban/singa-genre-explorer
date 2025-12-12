6266a92944f62dbc2a8312314f3053be36a97347
Habib Shaban <h.shaban234@yahoo.com>
2025-12-12 11:17:30 +0200

    feat: add API_BASE_URL to runtime configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  runtimeConfig: {
+    // Private keys are only available on the server
+    apiBaseUrl: process.env.API_BASE_URL || "https://api.singa.com/v1.4", // I'm keeping this for backward compatibility
+  },
   modules: ["@nuxt/icon", "@nuxt/ui"],
   css: ["~/assets/styles/main.css"],
 });
