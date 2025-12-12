04be1b6935115d2b034fd581feb3ed027797284d
Habib Shaban <h.shaban234@yahoo.com>
2025-12-12 11:43:31 +0200

    feat: update index page to display genres list

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,5 +5,19 @@ const { genres } = useGenres();
 </script>
 
 <template>
-  <div class="">Home page</div>
+  <div class="p-6">
+    <h1 class="text-3xl font-bold text-white mb-6">
+      Genres
+    </h1>
+  </div>
+
+  <ul class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4" role="list" aria-label="Genre list">
+    <li
+      v-for="genre in genres"
+      :key="genre.id"
+      :genre="genre"
+    >
+    {{ genre.name }}
+    </li>
+  </ul>
 </template>
