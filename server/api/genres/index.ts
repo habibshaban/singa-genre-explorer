cc22d3da5760f357db17dd32ccc065ee1e773ddc
Habib Shaban <h.shaban234@yahoo.com>
2025-12-14 10:07:38 +0200

    fix: enhance error handling and add timeout/retry options for genre fetching, and changing error since it's upstream error

--- server/api/genres/index.ts
+++ server/api/genres/index.ts
@@ -5,13 +5,20 @@ export default defineCachedEventHandler(
     const { apiBaseUrl } = useRuntimeConfig(event);
     const url = `${apiBaseUrl}/genres?page_size=1000`;
     try {
-      const genresResponse = await $fetch<SingaGenresResponse>(url);
+      const genresResponse = await $fetch<SingaGenresResponse>(url, {
+        // since we are using caching here, we can have a shorter timeout and retries
+        timeout: 5000,
+        retry: 2,
+        retryDelay: 500,
+      });
       return genresResponse;
-    } catch {
+    } catch (error) {
+      // Since we are fetching from external API, we should set status code to 502, for upstream errors
       throw createError({
-        statusCode: 500,
-        statusMessage: "Internal Server Error",
-        message: "Failed to fetch genres",
+        statusCode: 502,
+        statusMessage: "Bad Gateway",
+        message: "Invalid upstream response when fetching genres",
+        cause: error,
       });
     }
   },
